9e9c47c3a0d4fdd2da9ee3c20274b06e6cd1ab4b
Alexandre Koch <alexandre.koch@edu.devinci.fr>
2025-11-28 14:03:42 +0100

    Fix: Update test with correct ticker and supply values

--- test/MonERC20.t.sol
+++ test/MonERC20.t.sol
@@ -6,14 +6,14 @@ import {MonERC20} from "../src/MonERC20.sol";
 
 contract MonERC20Test is Test {
     MonERC20 public token;
-    uint256 public initialSupply = 810077968000000000000000000;
+    uint256 public initialSupply = 533983428000000000000000000;
 
     function setUp() public {
         token = new MonERC20(initialSupply);
     }
 
     function testTicker() public {
-        assertEq(token.symbol(), "sewpjhga");
+        assertEq(token.symbol(), "giw4o5i");
     }
 
     function testSupply() public {
